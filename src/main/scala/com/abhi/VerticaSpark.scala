62d4cadd1328efacc2b10718b5caf5f79fa7fb64
Abhishek Srivastava <abhishek.srivastava@dealertrack.com>
2016-01-12 20:44:16 -0600

    still does not work

--- src/main/scala/com/abhi/VerticaSpark.scala
+++ src/main/scala/com/abhi/VerticaSpark.scala
@@ -6,10 +6,11 @@ package com.abhi
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
-import org.apache.hadoop.io.LongWritable
+import org.apache.hadoop.io.{IntWritable, LongWritable, Text}
 import com.vertica.hadoop._
 import org.apache.hadoop.mapreduce._
 import org.apache.hadoop.conf.Configuration
+
 
 object VerticaSpark extends App {
   val scConf = new SparkConf
@@ -23,7 +24,30 @@ object VerticaSpark extends App {
 
   val job = new Job(conf)
   job.setInputFormatClass(classOf[VerticaInputFormat])
+  job.setOutputKeyClass(classOf[Text])
+  job.setOutputValueClass(classOf[VerticaRecord])
+  job.setOutputFormatClass(classOf[VerticaOutputFormat])
+
   VerticaInputFormat.setInput(job, "select * from Foo where key = ?", "1", "2", "3", "4")
-  val rdd : RDD[VerticaRecord] = sc.newAPIHadoopRDD(job.getConfiguration, classOf[VerticaInputFormat], classOf[LongWritable], classOf[VerticaRecord]).map(_._2)
-  rdd.collect().foreach(x => println(s"${x.get(0)} ${x.get(1)}"))
+  VerticaOutputFormat.setOutput(job, "Bar", true, "name varchar", "total int")
+  val rddVR : RDD[VerticaRecord] = sc.newAPIHadoopRDD(job.getConfiguration, classOf[VerticaInputFormat], classOf[LongWritable], classOf[VerticaRecord]).map(_._2)
+  // convert rdd of vertica records to rdd of tuples. first is name second is val
+  val rddTup = rddVR.map(x => (x.get(1).toString(), x.get(2).toString().toInt))
+  // group by
+  val rddGroup = rddTup.reduceByKey(_ + _)
+  val rddVROutput = rddGroup.map({
+    case(x, y) => (new Text("Bar"), getVerticaRecord(x, y, job.getConfiguration))
+  })
+
+  //rddVROutput.saveAsNewAPIHadoopFile("Bar", classOf[Text], classOf[VerticaRecord], classOf[VerticaOutputFormat], job.getConfiguration)
+  rddVROutput.saveAsNewAPIHadoopDataset(job.getConfiguration)
+
+  def getVerticaRecord(name : String, value : Int , conf: Configuration) : VerticaRecord = {
+    println("++++++++++++++ CAME INSIDE +++++++++++++++++ ")
+    var retVal = new VerticaRecord(conf)
+    //println(s"going to build Vertica Record with ${name} and ${value}")
+    retVal.set(0, new Text(name))
+    retVal.set(1, new IntWritable(value))
+    retVal
+  }
 }
